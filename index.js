dd195c92e7bcbb92e60f400276052486c769a256
MEGHAPRAJAPATI <114502216+MEGHAPRAJAPATI@users.noreply.github.com>
2024-03-24 19:46:23 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,24 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import { Component } from 'react';
-import {assets} from 'react';
-import {hooks} from 'react';
-import useNode from './hooks/useNode';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
 
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
-    <Component/>
-      
-      <ReactDOM/>
-      <useNode/>
-      </React.StrictMode>
+  </React.StrictMode>
 );
-
-
-reportWebVitals();
